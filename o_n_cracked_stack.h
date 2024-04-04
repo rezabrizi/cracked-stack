274ba234befd4fda534b8f6ddb405d5b999f7abd
Reza Tabrizi <rtabrizi03@gmail.com>
2024-04-04 18:24:19 -0500

    first commit

---
+++ o_n_cracked_stack.h
@@ -0,0 +1,54 @@
+
+#pragma once
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+class o_n_cracked_stack{
+
+public:
+    void push(int element)
+    {
+        stack.push_back(element);
+    }
+
+
+    int pop()
+    {
+        if (stack.empty())
+            throw std::runtime_error("stack is empty");
+        int top = stack.back();
+        stack.pop_back();
+        return top;
+    }
+
+    bool increment(size_t x, int v)
+    {
+
+        size_t n = stack.size();
+        size_t i = (n < x) ? 0 : n-x;
+
+        std::vector <int> temp (std::begin(stack)+i, stack.end());
+
+
+        for (size_t j = 0; j < temp.size(); j++)
+        {
+            if (INT_MAX - temp[j] > v)
+            {
+                temp[j] += v;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        std::copy(temp.begin(), temp.end(), stack.begin()+i);
+        return true;
+    }
+
+private:
+
+    std::vector<int> stack;
+};
+
